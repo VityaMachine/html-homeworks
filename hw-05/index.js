33e6bf96e56091ccc8d8496f5a6071f1c6e2d097
VityaMachineNoteWin <vatamanenko9@gmail.com>
2022-10-14 02:45:52 +0300

    list card creating process

--- hw-05/index.js
+++ hw-05/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+import data from "./src/data/places.json" assert { type: "json" };
+
+const ref = document.querySelector(".js-test-selector");
+
+
+const mkData = data[3];
+
+const markup = `
+<div class="title">${mkData.title}</div>
+<div class="descr">${mkData.description}</div>
+<img src="${mkData.image}" alt="${mkData.title}">
+`;
+
+
+
+ref.innerHTML = markup;
+
+
